92065305d7343b7208393b365a8f82e32dda1798
AdeeloD <162063596+AdeeloD@users.noreply.github.com>
2025-04-05 12:46:54 +0200

    .env

--- services/config/firebaseConfig.ts
+++ services/config/firebaseConfig.ts
@@ -3,14 +3,22 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID,
+} from '@env';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAirb_nI9VQyHD9PoqoTxIToeyTgQcuGRU",
-  authDomain: "meowmentor-97c17.firebaseapp.com",
-  projectId: "meowmentor-97c17",
-  storageBucket: "meowmentor-97c17.firebasestorage.app",
-  messagingSenderId: "967113875756",
-  appId: "1:967113875756:web:e221de7c7dc64ca1860c89"
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
